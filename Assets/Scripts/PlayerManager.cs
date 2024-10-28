5ae68f3c80bf6933417e6f8ba79881276de3baad
Neulainen <reksis02@outlook.com>
2024-10-28 23:06:30 +0200

    tekstuureja päivitetty

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public Rigidbody rb;
+    public BoxCollider mainColl;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        mainColl = GetComponent<BoxCollider>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
