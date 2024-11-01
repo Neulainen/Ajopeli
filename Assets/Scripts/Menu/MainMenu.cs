37da2c2e2290f5c435514df7c00b34ec5d8a851b
Neulainen <128168273+Neulainen@users.noreply.github.com>
2024-11-01 11:37:51 +0200

    fhfh

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,23 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button StartButton;
+    public Button Garagebutton;
+    public Button HelpButton;
+    public Button QuitButton;
     
     // Start is called before the first frame update
     void Start()
     {
+    
       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartButton.clicked += StartGame;
+        HelpButton.clicked += HelpPage;
+
     }
     void StartGame()
     {
+       
         SceneManager.LoadScene("Main");
     }
     void HelpPage()
@@ -29,4 +38,5 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+    
 }
